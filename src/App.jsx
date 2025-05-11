7dc65510ef9b74bc13ac2dd9a9053162aa59d0d1
Aakansha Thakur <98897384+AakanshaThakur@users.noreply.github.com>
2025-05-12 01:06:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import StateSelector from "./components/StateSelector";
+function App() {
+  return (
+    <div>
+      <StateSelector />
+    </div>
+  );
+}
+
+export default App;
